2f67493929ca9b95b5913e157359f5be77e94673
Anna Hyrylo <anna.archeer@gmail.com>
2018-02-20 22:08:14 +0200

    Update cypherPhrase.js

--- homework_09/js/cypherPhrase.js
+++ homework_09/js/cypherPhrase.js
@@ -1,1 +1,11 @@
-//cypherPhrase
+function cypherPhrase(obj, str){
+	var str = str.split("");
+	return getTransformedArray(obj, function(num){
+		Object.keys(obj).forEach(function(key){
+			if (num === key){
+				num = obj[num];
+			}
+		});
+		return(num);
+	}).join("");
+}
