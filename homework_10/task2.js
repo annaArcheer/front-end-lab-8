9425059e41ad3a05545528dcd02bac61f78b7274
Anna Hyrylo <anna.archeer@gmail.com>
2018-02-25 17:38:53 +0200

    Update task2.js

--- homework_10/task2.js
+++ homework_10/task2.js
@@ -40,29 +40,29 @@ function showResult(fighter) {
  * The following code must be valid after implementation!
  */ 
 
-// var fighter1 = fighter({name: 'John', attack: 100, hp: 100});
-// var fighter2 = fighter({name: 'Kayn', attack: 50, hp: 300});
-// var fighter3 = fighter({name: 'Bill', attack: 40, hp: 100});
+var fighter1 = fighter({name: 'John', attack: 100, hp: 100});
+var fighter2 = fighter({name: 'Kayn', attack: 50, hp: 300});
+var fighter3 = fighter({name: 'Bill', attack: 40, hp: 100});
 
-// fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
-// fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
+fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
+fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
 
-// /**
-//  * Fighter John
-//  * - Combat stats: { wins: 1, loses: 0 }
-//  * - Properties: { name: 'John', attack: 100, hp: 100 }
-//  */
-// showResult(fighter1);
+/**
+ * Fighter John
+ * - Combat stats: { wins: 1, loses: 0 }
+ * - Properties: { name: 'John', attack: 100, hp: 100 }
+ */
+showResult(fighter1);
 
-// /** Fighter Kayn
-//  * - Combat stats: { wins: 0, loses: 0 }
-//  * - Properties: { name: 'Kayn', attack: 50, hp: 200 }
-//  */
-// showResult(fighter2); 
+/** Fighter Kayn
+ * - Combat stats: { wins: 0, loses: 0 }
+ * - Properties: { name: 'Kayn', attack: 50, hp: 200 }
+ */
+showResult(fighter2); 
 
-// /**
-//  * Fighter Bill
-//  * - Combat stats: { wins: 0, loses: 1 }
-//  * - Properties: { name: 'Bill', attack: 40, hp: 0 }
-//  */
-// showResult(fighter3);
+/**
+ * Fighter Bill
+ * - Combat stats: { wins: 0, loses: 1 }
+ * - Properties: { name: 'Bill', attack: 40, hp: 0 }
+ */
+showResult(fighter3);
